dd6b44d3e9a6bba9b2f133add857221bcb4d290b
Ashutosh-Bhadauriya <ashutoshb455@gmail.com>
2023-08-06 20:14:03 +0530

    Add auth, landing and structure all files

--- components/shared/ContentWrapper.tsx
+++ components/shared/ContentWrapper.tsx
@@ -1,4 +1,10 @@
-export default function ContentWrapper({ children }: any) {
+import React from "react";
+
+interface ContentWrapperProps {
+  children: React.ReactNode;
+}
+
+export default function ContentWrapper({ children }: ContentWrapperProps) {
   return (
     <div className="min-h-screen flex flex-col p-8 md:px-12 gap-6 justify-center items-center">
       {children}

3d788f00463a26546d93e2658421e3b350a3c8b7
Ashutosh Bhadauriya <ashutoshb455@gmail.com>
2023-08-07 09:18:19 +0530

    Add auth, landing and structure all files (#1)
    * Add auth, landing and structure all files
    
    * modify gitignore
    
    * update env
    
    * Add favicon and make text lowercase

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import MainNav from "@/components/main-nav";
+import SiteFooter from "@/components/site-footer";
+
+interface LandingLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function LandingLayout({ children }: LandingLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col lowercase">
+      <header className="container z-40 bg-background px-32">
+        <div className="flex h-20 items-center justify-between">
+          <MainNav />
+          <nav>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "default", size: "sm" }),
+                'rounded-3xl'
+              )}
+            >
+              get started
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main className="flex-1">{children}</main>
+      <SiteFooter />
+    </div>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "Flickz",
+    url: "https://flickz.app",
+    // ogImage: "lorem ipsum",
+    description:
+      "Next genenration video creation platform",
+    links: {
+      github: "https://github.com/flickzapp/flickz",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
+  

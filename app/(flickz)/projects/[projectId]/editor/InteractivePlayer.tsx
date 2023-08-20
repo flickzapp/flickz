a35c06150073dbbced678fabb170f4941a174931
Ashutosh-Bhadauriya <ashutoshb455@gmail.com>
2023-08-20 21:23:36 +0530

    Feat: add beat sync

--- app/(flickz)/projects/[projectId]/editor/InteractivePlayer.tsx
+++ app/(flickz)/projects/[projectId]/editor/InteractivePlayer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useAudioData, visualizeAudio } from "@remotion/media-utils";
 
 import { cn } from "@/lib/utils";
 import {
@@ -9,7 +10,14 @@ import {
   interpolate,
   AbsoluteFill,
   Img,
+  Audio,
+  staticFile,
 } from "remotion";
+
+const hasSignificantPeak = (visualization: number[]): boolean => {
+  const threshold = 0.2;
+  return visualization.some((value) => value > threshold);
+};
 
 function FrameRenderer({
   ipframe,
@@ -28,7 +36,7 @@ function FrameRenderer({
     config: {
       damping: 200,
     },
-    durationInFrames: (ipframe.duration * fps) / 2,
+    durationInFrames: (ipframe.duration * fps) / 4,
   });
 
   const exitProgress = spring({
@@ -37,7 +45,7 @@ function FrameRenderer({
     config: {
       damping: 200,
     },
-    durationInFrames: (ipframe.duration * fps) / 2,
+    durationInFrames: (ipframe.duration * fps) / 4,
   });
 
   let animate = {
@@ -110,7 +118,7 @@ function FrameRenderer({
 
       <div
         className={cn(
-          `z-10 !bg-clip-text text-transparent !bg-cover !bg-center transition-all`,
+          `z-10 !bg-clip-text text-transparent !bg-cover !bg-center`,
           `font-${ipframe.fontFamily} ${ipframe.align} ${
             ipframe.fontWeight || "font-bold"
           } ${ipframe.fontSize || "text-9xl"}`
@@ -138,22 +146,41 @@ export default function InteractivePlayer({
   currentFrameText: string;
 }) {
   const { fps } = useVideoConfig();
+  const frame = useCurrentFrame();
+  const audioData = useAudioData(staticFile("dont-blink.mp3"));
+
+  if (!audioData) {
+    return null;
+  }
+
+  const visualization = visualizeAudio({
+    fps,
+    frame,
+    audioData,
+    numberOfSamples: 16,
+  });
+
+  const dynamicDuration = hasSignificantPeak(visualization) ? 0.5 : 1;
+
   return (
-    <Series>
-      {frames.map((ipframe: frameInputType, index: number) => {
-        return (
-          <Series.Sequence
-            key={index}
-            durationInFrames={ipframe.duration * fps}
-          >
-            <FrameRenderer
-              ipframe={ipframe}
-              currentFrame={currentFrame === index}
-              currentFrameText={currentFrameText}
-            />
-          </Series.Sequence>
-        );
-      })}
-    </Series>
+    <AbsoluteFill>
+      <Audio src={staticFile("dont-blink.mp3")} />
+      <Series>
+        {frames.map((ipframe: frameInputType, index: number) => {
+          return (
+            <Series.Sequence
+              key={index}
+              durationInFrames={ipframe.duration * fps * dynamicDuration}
+            >
+              <FrameRenderer
+                ipframe={ipframe}
+                currentFrame={currentFrame === index}
+                currentFrameText={currentFrameText}
+              />
+            </Series.Sequence>
+          );
+        })}
+      </Series>
+    </AbsoluteFill>
   );
 }
